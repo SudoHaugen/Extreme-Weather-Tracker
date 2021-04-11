313639a8f411889dcdf2a5952aae94c31dae7d39
Marius Haugen <Marius.io@protonmail.com>
2021-04-11 20:24:21 +0200

    Cards html
    Added placeholder html for event cards

--- src/Components/WeatherEvents.jsx
+++ src/Components/WeatherEvents.jsx
@@ -1,20 +1,33 @@
 /**@format */
 
+/** Displays simple cards with information about each weather event */
+
+import "../static_resources/event_cards/main.css";
+
 const WeatherEvents = ({ data }) => {
   let weatherEvents = [];
   data.forEach((element) => {
     weatherEvents.push(
-      <div>
-        <ul>
-          <li>Title: {element.title}</li>
-          <li>
-            {element.description !== null
-              ? "No description was given"
-              : element.description}
-          </li>
-          <li>Category: {element.category}</li>
-        </ul>
-        <br />
+      <div class="center">
+        <div class="property-card">
+          <a href="#">
+            <div class="property-image">
+              <div class="property-image-title">
+                <h5>Card Title</h5>
+              </div>
+            </div>
+          </a>
+          <div class="property-description">
+            <h5> Card Title </h5>
+            <p>
+              Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More
+              Bingo. Lorem Ipum doth be hard.
+            </p>
+          </div>
+          <a href="#">
+            <div class="property-social-icons"></div>
+          </a>
+        </div>
       </div>
     );
   });
