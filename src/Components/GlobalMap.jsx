6f8cce812e29a726f068bbaa2b988a49d34c8941
Marius Haugen <Marius.io@protonmail.com>
2021-04-12 21:41:25 +0200

    icons
    Added icons for weather events to be displayed on google maps

--- src/Components/GlobalMap.jsx
+++ src/Components/GlobalMap.jsx
@@ -1,17 +1,24 @@
 /**@format */
 
 import React from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
-import "../static_resources/maps/main.css";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+//import Marker from "./Marker";
+//import "../static_resources/maps/main.css";
+import wildfire_icon from "../static_resources/img/wildfire_icon_resized.jpg";
 
 const containerStyle = {
-  width: "1200px",
+  width: "1400px",
   height: "600px",
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523,
+  lat: 0,
+  lng: -119.417931,
+};
+
+const position = {
+  lat: 39.857563414,
+  lng: -119.417931,
 };
 
 function GlobalMap() {
@@ -41,7 +48,7 @@ function GlobalMap() {
       onUnmount={onUnmount}
     >
       {/* Child components, such as markers, info windows, etc. */}
-      <></>
+      <Marker icon={wildfire_icon} position={position} />
     </GoogleMap>
   ) : (
     <></>
---
+++ src/Components/Marker.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../static_resources/maps/map_icons/wildfire_main.css";
+
+const Marker = (props) => {
+    //const { title } = props;
+    return ( 
+        <div className="marker" />
+     );
+}
+ 
+export default Marker;
